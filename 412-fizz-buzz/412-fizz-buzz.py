d914802662682ddf9a7ada58ef95371a1fae6f29
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-05-06 13:02:55 -0500

    Time: 68 ms (29.40%), Space: 15.3 MB (5.09%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        result = []
+        for i in range(1, n + 1):
+            answer = ""
+            if i % 3 == 0:
+                answer += "Fizz"
+            if i % 5 == 0:
+                answer += "Buzz"
+            if answer == "":
+                answer = str(i)
+            result.append(answer)
+        return result
+            
