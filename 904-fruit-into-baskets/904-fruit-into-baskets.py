e9465f25da35d653eb7e145688d3f6521e68c42f
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-06-16 17:45:20 -0700

    Time: 1130 ms (66.73%), Space: 20 MB (87.77%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # init variables
+        fruits_in_basket = collections.Counter()
+        # fruit counts
+        curr_fruits = max_fruits = 0
+        # left tree
+        first_tree = 0
+        
+        # go through all fruits
+        for i in range(len(fruits)):
+            # expand the basket by 1
+            fruits_in_basket[fruits[i]] += 1
+            
+            # check that the basket is in range
+            while len(fruits_in_basket) > 2:
+                max_fruits = max(max_fruits, i - first_tree)
+                
+                fruits_in_basket[fruits[first_tree]] += -1
+                if fruits_in_basket[fruits[first_tree]] == 0:
+                    del fruits_in_basket[fruits[first_tree]]
+                    
+                # decrease the left bucket by 1
+                first_tree += 1
+                
+        # just in case checker for the last case  
+        max_fruits = max(max_fruits, i - first_tree + 1)
+        
+        return max_fruits
+                
+            
+            
+            
