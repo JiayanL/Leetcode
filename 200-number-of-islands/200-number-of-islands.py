67b2849e2cb826fb4a687266aa8a9d22256dda0d
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-07-01 16:59:36 -0700

    Time: 323 ms (88.68%), Space: 16.3 MB (90.40%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,22 +1,36 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        # edge cases - empty grid
         if not grid:
             return 0
-        
         count = 0
+        # bfs
+        # visit every point in the grid
         for y in range(len(grid)):
-            for x in range(len(grid[0])):
-                if grid[y][x] == '1':
-                    self.dfs(grid, y, x)
+            for x in range(len(grid[y])):
+                if grid[y][x] == "1":
+                    grid[y][x] = 0
                     count += 1
+                    self.bfs(grid, [y,x])
+                    
         return count
     
-    def dfs(self, grid, y, x):
-        if y < 0 or x < 0 or y >= len(grid) or x >= len(grid[0]) or grid[y][x] != '1':
-            return
-        grid[y][x] = '#'
-        self.dfs(grid, y + 1, x)
-        self.dfs(grid, y, x + 1)
-        self.dfs(grid, y -1 , x)
-        self.dfs(grid, y, x- 1)
+    def bfs(self, grid, coords) -> None:
+        # create a queue
+        queue = collections.deque([coords])
+        # add all points around it
+        while queue:
+            y, x = queue.popleft()
+            for i, j in [[y + 1, x], [y - 1, x], [y, x + 1], [y, x - 1]]:
+                if 0<=i<len(grid) and 0<=j<len(grid[i]) and grid[i][j] == "1":
+                    queue.append([i,j])
+                    grid[i][j] = 0
+            
+                
+        
+#     def dfs_iterative(self, grid, coords) -> None:
+#         pass
+    
+#     def dfs_recursive(self, grid, coords) -> None:
+#         pass
         
