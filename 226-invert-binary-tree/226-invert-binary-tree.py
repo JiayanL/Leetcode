97d2ee585192c7b8bc6f3ce7c5e666e1bd8583a4
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-07-05 13:43:31 -0700

    Time: 41 ms (68.25%), Space: 13.8 MB (56.45%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -6,6 +6,33 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        solution = "dfs"
+        if solution == "dfs":
+            return self.dfs(root)
+        if solution == "bfs":
+            pass
+        if solution == "recursive":
+            return self.recursive(root)
+        
+    def dfs(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root is None:
+            return root
+        
+        stack = [root]
+        
+        while stack:
+            curr = stack.pop()
+            curr.left, curr.right = curr.right, curr.left
+            if curr.right:
+                stack.append(curr.right)
+            if curr.left:
+                stack.append(curr.left)
+        return root
+    
+    def bfs(Self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        pass
+    
+    def recursive(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if root:
             root.left, root.right = self.invertTree(root.right), self.invertTree(root.left)
         return root
