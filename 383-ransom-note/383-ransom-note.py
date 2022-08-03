8b72af8450ac16f56f94d6269b9d12015a3c3bea
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-08-03 09:58:38 -0700

    Time: 84 ms (59.64%), Space: 14.1 MB (94.50%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,18 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         # get letters from magazine
-        letters = {}
-        for i in magazine:
-            if i not in letters:
-                letters[i] = 0
-            letters[i] += 1
+        letters = collections.Counter(magazine)
         
         # check letters in ransomNote
-        for i in ransomNote:
-            if i not in letters:
+        for c in ransomNote:
+            if letters[c] <= 0:
                 return False
-            letters[i] -= 1
-            if letters[i] < 0:
-                return False
+            letters[c] -= 1
         return True
             
