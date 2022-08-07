f44e92eb62febfc27f8b8b340ed021af67b24d05
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-08-07 13:28:53 -0700

    Time: 51 ms (69.34%), Space: 14.4 MB (27.02%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -5,74 +5,58 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
+from collections import deque
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        ## ✅ run iterative bfs
-        return self.iterativebfs(node)
-        ## ✅ run iterative dfs
-        return self.iterativedfs(node)
-        ## ✅ run recursive dfs
-        return self.recursivedfs(node)
-    
-    def iterativebfs(self, node: 'Node') -> 'Node':
-        # check that node is valid
+        method = "iDFS"
+        if method == "iBFS":
+            return self.iBFS(node)
+        elif method == "iDFS":
+            return self.iDFS(node)
+        elif method == "rDFS":
+            return self.rDFS(node)
+    def iBFS(self, node: 'Node') -> 'Node':
+        # dictionary to hold all the copies of each node
+        copyHolder = {}
+        
         if not node:
-            return node
-        # initialize variables 
-        # dictionary contains deep copy of each node
-        deep_copy = {node: Node(node.val)}
+            return 
         
-        # use a queue for bfs
-        queue = collections.deque([node])
+        # set up general BFS
+        queue = deque([node])
+        copyHolder[node] = Node(node.val)
         
-        # breadth first search
         while queue:
-            curr_node = queue.popleft()
-            # check if seen
-            for neighbor in curr_node.neighbors:
-                if neighbor not in deep_copy:
-                    deep_copy[neighbor] = Node(neighbor.val)
-                    queue.append(neighbor)
-                deep_copy[curr_node].neighbors.append(deep_copy[neighbor])
-        return deep_copy[node]
+            currNode = queue.popleft()
+            
+            for nNode in currNode.neighbors:
+                if nNode not in copyHolder: #not visited:
+                    queue.append(nNode)
+                    copyHolder[nNode] = Node(nNode.val)
+                copyHolder[currNode].neighbors.append(copyHolder[nNode])
         
-    def iterativedfs(self, node: 'Node') -> 'Node':
-        # check that node is valid
-        if not node:
-            return node
-        # initialize dictionary and stack(dfs)
-        deep_copy = {node: Node(node.val)}
+        return copyHolder[node]
+            
+    def iDFS(self, node: 'Node') -> 'Node':
+        visited = {}
         stack = [node]
+        if not node:
+            return None
+        visited[node] = Node(node.val)
         
-        # do dfs
         while stack:
-            curr_node = stack.pop()
-            for neighbor in curr_node.neighbors:
-                if neighbor not in deep_copy:
-                    stack.append(neighbor)
-                    deep_copy[neighbor] = Node(neighbor.val)
-                deep_copy[curr_node].neighbors.append(deep_copy[neighbor])
-        # return node
-        return deep_copy[node]
+            curr = stack.pop()
+            for nNode in curr.neighbors:
+                if nNode not in visited:
+                    visited[nNode] = Node(nNode.val)
+                    stack.append(nNode)
+                visited[curr].neighbors.append(visited[nNode])
+        return visited[node]
     
-    def recursivedfs(self, node: 'Node') -> 'Node':
-        def dfs(node: 'Node'):
-            if not node:
-                return None
-            # return node if visited
-            elif node in deep_copy:
-                return deep_copy[node]
-            # visit node
-            deep_copy[node] = Node(node.val)
-            # visit neighbors
-            for neighbor in node.neighbors:
-                print(neighbor)
-                deep_copy[node].neighbors.append(dfs(neighbor))
-            return deep_copy[node]
-        # dictionary contains deep copy of each node
-        deep_copy = {}
-        return dfs(node)
+    def rDFS(self, node: 'Node') -> 'Node':
+        pass
+    
+
     
             
         
