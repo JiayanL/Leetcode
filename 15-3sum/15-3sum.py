b3493c0c9c8340b32da0f1727b282be82bbbc234
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-08-07 00:28:51 -0700

    Time: 1578 ms (40.64%), Space: 18.2 MB (22.07%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,50 +1,37 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # sort array
         nums.sort()
-        result = []
+        triplets = []
+        i = 0
         
-        # loop through each element in the arary, skipping duplicates
-        for i in range(len(nums) - 2):
-            
-            # fastforward duplicates
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-        
-            # for each element, initialize a left and a right pointer, skipping duplicates
-            l, r = i + 1, len(nums) - 1
-        
-            while l < r:
-                    
-                # add up the three items
-                three_sum = nums[l] + nums[r] + nums[i]
-
-                # if greater than 0:
-                # move the right pointer left, skipping duplicates
-                # making sure there's no overlap
-                if three_sum > 0:
-                    r -= 1
-
-                # if less than 0
-                # move the left pointer right, skipping duplicates
-                # making sure there's no overlap
-                elif three_sum < 0:
-                    l += 1
-                    
-                # if equal 0
-                # add the triplet to my results, and 
-                # the left pointer over one if possible
-                elif three_sum == 0:
-                    result.append([nums[i], nums[l], nums[r]])
-                    
-                    # move to the end of duplicates
-                    while l + 1 < r and nums[l] == nums[l + 1]:
-                        l += 1
-                    while r - 1 > l and nums[r] == nums[r - 1]:
-                        r -= 1
-                        
-                    # adjust since every k can noly have one number
-                    
-                    l += 1
-                    r -= 1
-        return result
+        while i < len(nums) - 2:
+            # set up two pointers method
+            left = i + 1
+            right = len(nums) -1
+            while left < right:
+                # check that they're equal
+                target = nums[i] + nums[left] + nums[right]
+                
+                if target == 0:
+                    triplets.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                
+                # move right down
+                elif target > 0:
+                    right -= 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                
+                # move left up
+                elif target < 0:
+                    left += 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                
+            # move i to the next unique item
+            i += 1
+            while i < len(nums) and nums[i] == nums[i - 1]:
+                i += 1
+        return triplets
