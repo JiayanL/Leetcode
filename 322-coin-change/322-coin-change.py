b1658b6d959654e667bc0a1eb7a1191aaea6cf36
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-08-09 23:17:11 -0700

    Time: 1942 ms (68.86%), Space: 14.3 MB (62.52%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -4,13 +4,13 @@ from collections import deque
 class Solution:
     # top down DP with memoization
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [sys.maxsize] * (amount + 1)
+        # memo
+        dp = [float("inf")] * (amount + 1)
         dp[0] = 0
         
         for a in range(1, amount + 1):
             for c in coins:
-                # find previous coin
                 if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a-c])
+                    dp[a] = min(dp[a], dp[a-c] + 1)
         
-        return dp[amount] if dp[amount] != sys.maxsize else -1
+        return dp[amount] if dp[amount] != float("inf") else -1
