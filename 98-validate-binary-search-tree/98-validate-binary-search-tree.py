af55fc25e8da28de13a8b3d464757431f2d07a9e
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-08-10 17:11:29 -0700

    Time: 60 ms (69.95%), Space: 16.6 MB (46.02%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -12,11 +12,14 @@ class Solution:
         return (self.isValid(root))
         
     def isValid(self, node, low=-math.inf, high=math.inf) -> bool:
+        # if I've reached the end, it's balanced
         if not node:
             return True
         
-        if node.val <= low or node.val >= high:
+        # check that my node is between the low and highs for this part of the branch
+        if not low < node.val < high:
             return False
         
-        return (self.isValid(node.left, low, node.val) and 
-                self.isValid(node.right, node.val, high))
+        # adjust limits - when I move left, the new high is current val
+        # when I move right, the new low, is current low
+        return (self.isValid(node.left, low, node.val) and self.isValid(node.right, node.val, high))
