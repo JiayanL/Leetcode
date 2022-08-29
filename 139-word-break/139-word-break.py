0adba6882a543dd79d9d7f207e0ba83ca0f01cd1
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-08-29 09:48:52 -0700

    Time: 54 ms (66.04%), Space: 15.6 MB (5.00%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -1,15 +1,37 @@
+class TrieNode:
+    def __init__(self):
+        self.isWord = False
+        self.children = defaultdict(TrieNode)
+    def addWord(self, word):
+        curr = self
+        for c in word:
+            curr = curr.children[c]           
+        curr.isWord = True
+        
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # top down initialization
         n = len(s)
-        # keep track of if valid word ends at dp[i]
-        dp = [False for i in range(n + 1)]
+        # memoization, n + 1 for base case (first word we see)
+        dp = [False] * (n + 1)
+        dp[n] = True
+        root = TrieNode()
         
-        # this validates first word
-        dp[0] = True
+        # create trie
+        for word in wordDict:
+            root.addWord(word)
         
-        for i in range(1, n + 1):
-            for word in wordDict:
-                # check that previous word is okay AND that current word is in dictionary
-                if dp[i - len(word)] and s[i-len(word):i] == word:
+        # DP -> find word
+        for i in range(n-1, -1, -1):
+            curr = root
+            # from the current letter to the last letter
+            for j in range(i+1, n+1):
+                # check if this is in the word
+                c = s[j-1]
+                if c not in curr.children: break
+                else:
+                    curr = curr.children[c]
+                # check if I've reached a new word
+                if curr.isWord and dp[j]:
                     dp[i] = True
-        return dp[-1]
+        return dp[0]
