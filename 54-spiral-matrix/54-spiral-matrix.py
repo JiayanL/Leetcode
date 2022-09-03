83c2412f6fbb14b6799fc2b7da74c4ab35ce554f
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-03 14:16:51 -0700

    Time: 42 ms (67.97%), Space: 13.9 MB (33.53%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -2,32 +2,36 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         if len(matrix) == 0:
             return []
-        # Initialize row beginning and row end
+        
         result = []
         row_start, row_end = 0, len(matrix) - 1
         column_start, column_end = 0, len(matrix[0]) - 1
         
-        # Move all the way right, then down, then left, then up
-        # Repeat until I've hit the end
+        # go all the way right, then all the way down, then all the way left, then all the way up
         while row_start <= row_end and column_start <= column_end:
-            # Move across first row
+            
+            # all the awy right
             for i in range(column_start, column_end + 1):
                 result.append(matrix[row_start][i])
             row_start += 1
             
-            # Move down last column
+            # all the way down
             for i in range(row_start, row_end + 1):
                 result.append(matrix[i][column_end])
             column_end -= 1
             
+            # all the way left
+            # one more check
             if row_start <= row_end:
                 for i in range(column_end, column_start - 1, -1):
                     result.append(matrix[row_end][i])
                 row_end -= 1
             
+            # all the way up
             if column_start <= column_end:
-                for i in range(row_end, row_start -1, -1):
+                for i in range(row_end, row_start - 1, -1):
                     result.append(matrix[i][column_start])
                 column_start += 1
-                
+            
         return result
+                        
