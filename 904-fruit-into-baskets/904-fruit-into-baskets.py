ea560105baff09a6b5adf251c693ae93f04161cd
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-05 09:01:59 -0700

    Time: 2043 ms (11.16%), Space: 20.4 MB (27.14%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.py
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -1,33 +1,30 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        # init variables
-        fruits_in_basket = collections.Counter()
-        # fruit counts
-        curr_fruits = max_fruits = 0
-        # left tree
-        first_tree = 0
+        # 2 types of fruit
+        # pick every fruit
+        # longest substring with 2 elements
         
-        # go through all fruits
-        for i in range(len(fruits)):
-            # expand the basket by 1
-            fruits_in_basket[fruits[i]] += 1
-            
-            # check that the basket is in range
-            while len(fruits_in_basket) > 2:
-                # max_fruits = max(max_fruits, i - first_tree)
-                
-                fruits_in_basket[fruits[first_tree]] += -1
-                if fruits_in_basket[fruits[first_tree]] == 0:
-                    del fruits_in_basket[fruits[first_tree]]
-                    
-                # decrease the left bucket by 1
-                first_tree += 1
-            max_fruits = max(max_fruits, i - first_tree + 1)
-        # just in case checker for the last case (inclusive of the last case)
-        # max_fruits = max(max_fruits, i - first_tree + 1)
+        # initialize variables
+        fruit_basket = collections.Counter()
+        max_fruits = 0
+        start = 0
         
+        # move through fruits
+        for end in range(len(fruits)):
+            # add fruit to basket
+            fruit_basket[fruits[end]] += 1
+
+            # while I have more fruit than I need, shrink my basket
+            while len(fruit_basket) > 2:
+                remove_fruit = fruits[start]
+                fruit_basket[remove_fruit] -= 1
+                if fruit_basket[remove_fruit] == 0:
+                    del fruit_basket[remove_fruit]
+                start += 1
+
+            # count the amount of fruit I have right now
+            max_fruits = max(max_fruits, end - start + 1)
+
         return max_fruits
-                
-            
             
             
