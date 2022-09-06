d9d929b28bfc717ca17d647d72b343ac00d14de2
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-06 02:47:59 -0700

    Time: 426 ms (11.15%), Space: 53.4 MB (57.43%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,32 +6,12 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        
-        def arrayToTree(left_index, right_index):
-            # left and right index tell us the part of the subtree we're working with
-            nonlocal preorder_index
-            
-            # check if this is still valid
-            if left_index > right_index:
-                return None
-            
-            current_root = preorder[preorder_index]
-            preorder_index += 1
-            root = TreeNode(current_root)
-            # create a node with my current root
-            
-            # add the left and right for my current root
-            root.left = arrayToTree(left_index, inorder_root_index[current_root] - 1)
-            root.right = arrayToTree(inorder_root_index[current_root] + 1, right_index)
-            
+        # shorter version, same idea
+        if inorder:
+            root_val = preorder.pop(0)
+            inorder_index = inorder.index(root_val)
+            root = TreeNode(root_val)
+            root.left = self.buildTree(preorder, inorder[0:inorder_index])
+            root.right = self.buildTree(preorder,inorder[inorder_index + 1:])
             return root
-        # keep track of the root
-        preorder_index = 0
-        inorder_root_index = {}
-        
-        # keep track of where each root is in the inorder list
-        for i in range(len(inorder)):
-            inorder_root_index[inorder[i]] = i
-        
-        return arrayToTree(0, len(preorder) - 1)
-        
+        return None
