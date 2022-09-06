c3e06717d140c5fe182b4865e043cf5538768487
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-06 03:07:40 -0700

    Time: 389 ms (5.02%), Space: 14.8 MB (51.14%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,27 +1,25 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        i = j = 0
-        result = []
+        first_index, second_index = 0, 0
+        intersections = []
 
-        while i < len(firstList) and j < len(secondList):
-            # check if a overlaps b
-            a_overlaps_b = (firstList[i][0] >= secondList[j][0] and 
-                            firstList[i][0] <= secondList[j][1])
-            # check if b overlaps a
-            b_overlaps_a = (secondList[j][0] >= firstList[i][0] and
-                            secondList[j][0] <= firstList[i][1])
+        # while loop to step through both lists
+        while first_index < len(firstList) and second_index < len(secondList):
+            first_interval = firstList[first_index]
+            second_interval = secondList[second_index]
 
-            # if overlap, insert overlapping interval
-            if a_overlaps_b or b_overlaps_a:
-                result.append([max(firstList[i][0], secondList[j][0]), 
-                              min(firstList[i][1], secondList[j][1])])
+            # check if there's an intersection (if the start is between the other intersection)
+            if second_interval[0] <= first_interval[0] <= second_interval[1] or \
+            first_interval[0] <= second_interval[0] <= first_interval[1]:
+                start = max(first_interval[0], second_interval[0])
+                end = min(first_interval[1], second_interval[1])
+                # add the intersected interval (max of the start, min of the ends)
+                intersections.append([start, end])
 
-            # move to the interval that finishes first
-            if firstList[i][1] < secondList[j][1]:
-                i += 1
+            # move to the next interval (the one with the earlier end)
+            if first_interval[1] < second_interval[1]:
+                first_index += 1
             else:
-                j += 1
+                second_index += 1
 
-        return result
-    
-    
+        return intersections
