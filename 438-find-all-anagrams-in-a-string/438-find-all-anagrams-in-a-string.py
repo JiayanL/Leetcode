d6c320bcec619f8970d3278012894c2ad91e6dbf
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-08 04:49:35 -0700

    Time: 295 ms (38.41%), Space: 15.1 MB (81.35%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,48 +1,37 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        # similar to finding one anagram but instead of returning true, we return an array
+        # keep track of all letters in p
+        chars_to_match = collections.Counter(p)
         
-        # pattern: a substring of s, s_i is an anagram of p if it's the same length AND has all the same characters
+        # easy way to check if all the characters are satisfied in the right frequency
+        matched_chars = len(chars_to_match)
+        substring_length = len(p)
         
-        # keep track of the amount of matched characters we have
-        matched_chars = 0
-        # keep track of the characters
-        char_count = {}
-        # keep track of our starting window
-        window_start = 0
-        # keep track of indices of all our anagrams
+        start, end = 0, 0
         result = []
+        # keep track of the amount of letters I've matched
         
-        # now populate char_count
-        for char in p:
-            if char not in char_count:
-                char_count[char] = 0
-            char_count[char] += 1
-        
-        # find all anagrams
-        for window_end in range(len(s)):
-            right_char = s[window_end]
-            
-            # check that this character composes an anagram
-            if right_char in char_count:
-                char_count[right_char] -= 1
-                # check that we have a complete char match
-                if char_count[right_char] == 0:
-                    matched_chars += 1
-            
-            # check that we've matched all characters
-            if matched_chars == len(char_count):
-                result.append(window_start)
-            
-            # make sure we don't exceed the length of the pattern - this is the case as long as we've surpassed the length of the pattern
-            if (window_end + 1) >= len(p):  
-                first_char = s[window_start]
-                window_start += 1                
-                
-                # add it back to the hash map and decrement complete match
-                if first_char in char_count:
-                    if char_count[first_char] == 0:
-                        matched_chars -= 1
-                    char_count[first_char] += 1
+        # sliding window
+        for end in range(len(s)):
+            # process the current character
+            char = s[end]
+            if char in chars_to_match:
+                chars_to_match[char] -= 1
+                if chars_to_match[char] == 0:
+                    matched_chars -= 1
                     
+            if (end - start + 1) != substring_length:
+                continue
+                
+            # when all letters in my window match p, append the start
+            if matched_chars == 0:
+                result.append(start)
+            
+            # move the window left by one
+            start_char = s[start]
+            if start_char in chars_to_match:
+                if chars_to_match[start_char] == 0:
+                    matched_chars += 1
+                chars_to_match[start_char] += 1
+            start += 1
         return result
