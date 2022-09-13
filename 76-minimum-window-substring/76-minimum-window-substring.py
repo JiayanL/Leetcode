8d31a2eae288dbc650d9dba1bb2039b5c4600de2
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-13 21:43:32 +0200

    Time: 310 ms (24.48%), Space: 14.6 MB (83.75%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.py
+++ 76-minimum-window-substring/76-minimum-window-substring.py
@@ -1,39 +1,38 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        # init tracker of window start and character frequency
+        # count all characters in t
+        chars_in_t = collections.Counter(t)
+        matched_chars = len(chars_in_t)
+        
+        min_window_length = float(inf)
+        min_window_start = 0
+        
         window_start = 0
-        matched_chars = 0 
-        char_frequency = {}
         
-        min_length = len(s) + 1
-        result = ""
-        
-        # track the frequency of my characters
-        for char in t:
-            if char not in char_frequency:
-                char_frequency[char] = 0
-            char_frequency[char] += 1
-            
-        # go through s until we have matched
+        # move through all windows
         for window_end in range(len(s)):
-            right_char = s[window_end]
+            char = s[window_end]
             
-            # check if right_char is one of the characters we're looking for
-            if right_char in char_frequency:
-                char_frequency[right_char] -= 1
-                if char_frequency[right_char] == 0:
-                    matched_chars += 1
-                    
-            # decrease the window size while I have complete matches
-            while matched_chars == len(char_frequency):
-                # check that we have a smaller elngth
-                if (window_end - window_start + 1) < min_length:
-                    min_length = window_end - window_start + 1
-                    result = s[window_start:window_end + 1]
-                left_char = s[window_start]
+            if char in chars_in_t:
+                chars_in_t[char] -= 1
+                if chars_in_t[char] == 0:
+                    matched_chars -= 1
+            
+            # shrink it
+            while matched_chars == 0:
+                window_length = window_end - window_start + 1
+                if window_length < min_window_length:
+                    min_window_length = window_length
+                    min_window_start = window_start
+
+                start_char = s[window_start]
+                if start_char in chars_in_t:
+                    if chars_in_t[start_char] == 0:
+                        matched_chars += 1
+                    chars_in_t[start_char] += 1
                 window_start += 1
-                if left_char in char_frequency:
-                    if char_frequency[left_char] == 0:
-                        matched_chars -= 1
-                    char_frequency[left_char] += 1
-        return result
+                    
+    
+        if min_window_length == float(inf):
+            return ""
+        return s[min_window_start: min_window_start + min_window_length]
