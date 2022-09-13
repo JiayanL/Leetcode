a22d05d1d6f749440e444d353e690ce69b380d77
JiayanL <76187440+JiayanL@users.noreply.github.com>
2022-09-13 22:08:27 +0200

    Time: 677 ms (19.05%), Space: 16.5 MB (48.08%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,29 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         islandCount = 0
+        HEIGHT = len(grid)
+        WIDTH = len(grid[0])
         
-        if not grid:
-            return islandCount
+        def bfs(row, column):
+            
+            moves = [(1,0), (-1,0), (0,1), (0, -1)]
+            for y, x in moves:
+                newRow, newColumn = row + y, column + x
+                if 0 <= newRow < HEIGHT and 0 <= newColumn < WIDTH and grid[newRow][newColumn] == "1":
+                    grid[newRow][newColumn] = "#"
+                    bfs(newRow, newColumn)
         
-        rows = len(grid)
-        columns = len(grid[0])
-        
-        for i in range(rows):
-            for j in range(columns):
-                if grid[i][j] == "1":
-                    grid[i][j] = "#"
+        for row in range(HEIGHT):
+            for column in range(WIDTH):
+                if grid[row][column] == '1':
                     islandCount += 1
-                    self.bfs(grid, i, j)
-        return islandCount
-    
-    def bfs(self, grid: List[List[str]], i: int, j: int) -> None:
-        queue = collections.deque([(i, j)])
-        d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+                    grid[row][column] = "#"
+                    bfs(row, column)
         
-        while queue:
-            i, j = queue.popleft()
-            for dX, dY in d:
-                newX, newY = i + dX, j + dY
-                if 0 <= newX < len(grid) and 0 <= newY < len(grid[0]) and grid[newX][newY] == "1":              
-                    grid[newX][newY] = "#"
-                    queue.append((newX, newY))
+        return islandCount
